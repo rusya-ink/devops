0055818197fc1b732035ef6a05aed566e11265c5
rusya-ink <43722585+rusya-ink@users.noreply.github.com>
2021-08-24 17:00:51 +0300

    Lab 1
    - Add python web app showing time in Moscow
    - Add description of best practices

---
+++ app.py
@@ -0,0 +1,27 @@
+import datetime
+import redis
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+TIME_FORMAT = '%H:%M:%S'
+
+# Moscow is in UTC+3 timezone
+MOSCOW_UTC = 3
+
+
+@app.route('/')
+def hello():
+    current_time = datetime.datetime.now(tz=get_timezone_from_utc(MOSCOW_UTC))
+    return render_template('time_template.html',
+                           time=current_time.strftime(TIME_FORMAT))
+
+
+def get_timezone_from_utc(utc):
+    return datetime.timezone(datetime.timedelta(hours=utc))
+
+
+if __name__ == '__main__':
+    app.run()
+
