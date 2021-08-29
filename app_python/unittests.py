d10d7cc607036b51d2ced6ab06e9c702975f48a0
rusya-ink <43722585+rusya-ink@users.noreply.github.com>
2021-08-29 18:36:11 +0300

    fix test

--- app_python/unittests.py
+++ app_python/unittests.py
@@ -7,11 +7,12 @@ class AppTests(unittest.TestCase):
     def test_utc(self):
         test_cases = [-1000, -13, -12, -11, -3, 5, 13, 14, 15, 1000]
         for test_utc in test_cases:
-            result = get_timezone_from_utc(test_utc)
+            result = tested_app.get_timezone_from_utc(test_utc)
             if test_utc > 14 or test_utc < -12:
                 self.assertEqual(result, nil)
             else:
-                self.assertEqual(result, datetime.timezone(datetime.timedelta(hours=test_utc)))
+                self.assertEqual(result, datetime.timezone(
+                        datetime.timedelta(hours=test_utc)))
 
 
 if __name__ == '__main__':
