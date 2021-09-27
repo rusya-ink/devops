4f62925b9b6caaf35f3ae9126b24cf05c05c93ea
rusya-ink <43722585+rusya-ink@users.noreply.github.com>
2021-09-27 18:54:47 +0300

    add visits

--- app_python/app.py
+++ app_python/app.py
@@ -13,8 +13,18 @@ MOSCOW_UTC = 3
 @app.route('/')
 def hello():
     current_time = datetime.datetime.now(tz=get_timezone_from_utc(MOSCOW_UTC))
+    f = open("../k8s/my-app/files/visits-list.txt", "a")
+    f.write(current_time.strftime(TIME_FORMAT)+"/n")
+    f.close()
     return render_template('time_template.html',
                            time=current_time.strftime(TIME_FORMAT))
+
+@app.route('/visits')
+def visits():
+    f = open("../k8s/my-app/files/visits-list.txt", "r")
+    text = f.read()
+   return render_template('visits_template.html',
+                           visits=text) 
 
 
 def get_timezone_from_utc(utc):
